144e825b344e0f8f63eddd6cc4ba07a95a8ebcaa
Natanael Lourenço <holasoycael@gmail.com>
2022-05-16 09:04:14 -0300

    Test with japa failed

--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -3,12 +3,32 @@ import supertest from 'supertest'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
+/**
+  {
+    "users": {
+      "id": number
+      "email": string
+      "username": string
+      "password": string
+      "avatar": string
+    }
+  }
+*/
+
 test.group('User', () => {
-  test.only('its should create an user', async () => {
-    const userPayload = { email: 'test@test.com', username: 'test', password: 'test' }
-
-    console.log('BASE_URL', BASE_URL)
-
-    await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+  test.only('its should create an user', async (assert) => {
+    const userPayload = {
+      email: 'test@test.com',
+      username: 'test',
+      password: 'test',
+      avatar: 'https://images.com/image/1',
+    }
+    const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+    assert.exists(body.user, 'User undefined')
+    assert.exists(body.id, 'Id undefined')
+    assert.equal(body.email, userPayload.email)
+    assert.equal(body.username, userPayload.username)
+    assert.equal(body.password, userPayload.password)
+    assert.equal(body.avatar, userPayload.avatar)
   })
 })
