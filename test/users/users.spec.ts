2f12ac978213a958ffeca090eeceb94d12986983
Natanael Lourenço <holasoycael@gmail.com>
2022-05-18 06:47:37 -0300

    Test is passed from provide require data

--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -75,6 +75,13 @@ test.group('User', (group) => {
     assert.equal(body.status, 409)
   })
 
+  test.only('it should return 422 when required data is not provided', async (assert) => {
+    const { body } = await supertest(BASE_URL).post('/users').send({}).expect(422)
+
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
+  })
+
   group.beforeEach(async () => {
     await Database.beginGlobalTransaction()
   })
